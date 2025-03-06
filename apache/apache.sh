5ca6c91992b9d7dfc685285165b1fe7b8134a79f
NeonPotatoChips <32785705+NeonPotatoChips@users.noreply.github.com>
2025-03-05 19:22:04 -0500

    Update apache.sh

--- apache/apache.sh
+++ apache/apache.sh
@@ -34,3 +34,25 @@ cp index.html /usr/local/apache2/htdocs/index.html
 cp save_character.php /usr/local/apache2/htdocs/save_character.php
 cp quest.php /usr/local/apache2/htdocs/quest.php
 cp debug.log /usr/local/apache2/htdocs/debug.log
+
+#download php from source
+echo "[*] Setting up php"
+sudo apt install libxml2-dev libsqlite3-dev libcurl4-openssl-dev libonig-dev zlib1g-dev pkg-config -y
+cd /usr/local/src
+sudo wget https://www.php.net/distributions/php-8.1.0.tar.gz
+sudo tar -xzf php-8.1.0.tar.gz
+cd php-8.1.0
+
+sudo ./configure --with-apxs2=/usr/local/apache2/bin/apxs --with-mysqli --enable-mbstring --enable-xml --enable-cgi
+sudo make -j$(nproc)
+sudo make install
+
+#add module to config file
+echo "LoadModule php_module modules/libphp.so
+AddHandler application/x-httpd-php .php
+DirectoryIndex index.php index.html" >> /usr/local/apache2/conf/httpd.conf
+
+#restart apache service
+/usr/local/apache2/bin/apachectl restart
+
+
